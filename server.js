61c56209f33468c5772be8a8fbc31ad4b7711744
mary0077 <mgs35@discente.ifpe.edu.br>
2023-07-12 23:26:47 -0300

    atualizações

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+// Importe os módulos necessários
 const express = require('express');
 const app = express();
-const port = 3090;
+const path = require('path');
+const port = 3011;
 
 app.use(express.static('public'));
 
+// Configure a engine de visualização
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Defina a rota para a home
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.post('/consulta', (req, res) => {
+// Realize a consulta na base de dados com base nos parâmetros recebidos
+const resultados = consultarDados(req.body.filtro); // Exemplo hipotético
+
+res.render('resultado', { dados: resultados }); // Renderiza a view "resultado" passando os dados resultantes da consulta
+});
+
+// Resto do código do servidor
+
+// Inicie o servidor
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
-
